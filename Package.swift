dbccdee0a34f49f8ababddc4fceb7921a5290f40
Zach Eriksen <zmeriksen@icloud.com>
2022-04-29 19:28:41 -0600

    Downgrade to Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/0xOpenBytes/t", from: "0.2.0"),
         .package(url: "https://github.com/0xOpenBytes/c", from: "0.2.0"),
-        .package(url: "https://github.com/0xOpenBytes/o", from: "0.1.0")
+        .package(url: "https://github.com/0xOpenBytes/o", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
