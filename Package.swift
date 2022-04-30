35a4a21a058661e59556c2870e5ed2351f58622f
Zach Eriksen <zmeriksen@icloud.com>
2022-04-30 15:36:32 -0600

    Update packages

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/0xOpenBytes/t", from: "0.2.0"),
-        .package(url: "https://github.com/0xOpenBytes/c", from: "0.2.0"),
-        .package(url: "https://github.com/0xOpenBytes/o", from: "0.2.0")
+        .package(url: "https://github.com/0xOpenBytes/c", from: "0.3.0"),
+        .package(url: "https://github.com/0xOpenBytes/o", from: "0.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
